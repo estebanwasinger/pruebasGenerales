d76c9a55d75d0395b294a27100ea43e59416b21a
estebanwasinger <estebanwasinger@gmail.com>
2014-05-25 00:46:10 -0300

    Actualizado el prueba servidor-cliente

--- servidor/Debug/src/servidor.d
+++ servidor/Debug/src/servidor.d
@@ -1,1 +1,16 @@
-src/servidor.d: ../src/servidor.c
+src/servidor.d: ../src/servidor.c \
+ /home/utnso/workspace/tp-2014-1c-bar-c-lona/pruebaShared/socket.h \
+ /home/utnso/workspace/tp-2014-1c-bar-c-lona/pruebaShared/package.h \
+ /home/utnso/workspace/tp-2014-1c-bar-c-lona/pruebaShared/estructurasPackage.h \
+ /home/utnso/workspace/tp-2014-1c-bar-c-lona/pruebaShared/package.h \
+ /home/utnso/workspace/tp-2014-1c-bar-c-lona/pruebaShared/estructurasPackage.h
+
+/home/utnso/workspace/tp-2014-1c-bar-c-lona/pruebaShared/socket.h:
+
+/home/utnso/workspace/tp-2014-1c-bar-c-lona/pruebaShared/package.h:
+
+/home/utnso/workspace/tp-2014-1c-bar-c-lona/pruebaShared/estructurasPackage.h:
+
+/home/utnso/workspace/tp-2014-1c-bar-c-lona/pruebaShared/package.h:
+
+/home/utnso/workspace/tp-2014-1c-bar-c-lona/pruebaShared/estructurasPackage.h:
--- servidor/src/servidor.c
+++ servidor/src/servidor.c
@@ -10,23 +10,24 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <commons/socket.h>
-#include <commons/package.h>
-#include <commons/estructurasPackage.h>
+#include <socket.h>
+#include <package.h>
+#include <estructurasPackage.h>
+
+
 int main(void) {
 
 int socketEscuchaGeneral;
 int socketEscuchaNueva;
 
-socketEscuchaGeneral = socket_crearServidor("127.0.0.1",5555);
-socketEscuchaNueva = socket_aceptarCliente(socketEscuchaGeneral);
-
 void * estructuraRecibida;
 t_tipoEstructura tipoRecibido;
 
+socketEscuchaGeneral = socket_crearServidor("127.0.0.1",5555);
+socketEscuchaNueva = socket_aceptarCliente(socketEscuchaGeneral);
 
 socket_recibir(socketEscuchaNueva, &tipoRecibido, &estructuraRecibida);
-printf("%d",((t_struct_posxposy*) estructuraRecibida)->posX);
+printf("\nID: %d \n",((t_struct_handshake_umv*) estructuraRecibida)->tipoProceso);
 
 return 0;
 
